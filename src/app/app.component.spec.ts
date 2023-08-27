f7ffe1ac551259ba9d0643ae0f16594ac0280c07
Erick Yahir Meza Espina <espinayahir@gmail.com>
2023-08-27 12:53:04 -0600

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'auth-jello'`, () => {
+  it(`should have as title 'auth-yt'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('auth-jello');
+    expect(app.title).toEqual('auth-yt');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('auth-jello app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('auth-yt app is running!');
   });
 });
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-auth-85f25',
+    appId: '1:585227448750:web:51879b804dcabf91fb183f',
+    storageBucket: 'angular-auth-85f25.appspot.com',
+    apiKey: 'AIzaSyAgmYM5IkrPiNsdFl8lp8PKYaZLhi7Onww',
+    authDomain: 'angular-auth-85f25.firebaseapp.com',
+    messagingSenderId: '585227448750',
+  },
+};
